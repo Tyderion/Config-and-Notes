ddc2b7ae749be2dd14aa1a61a8b2d78b94fb019d
Gabriel Nadler <nadlergabriel@gmail.com>
2012-10-29 22:14:51 +0100

    PowerShell-Script now writes a File which is successfully read by Sublime Text 2 for windows.

--- Sublime/sublime-packages-windows.ps1
+++ Sublime/sublime-packages-windows.ps1
@@ -1,14 +1,19 @@
+# Script Variables
 $default_sublime_packages_path = "W:\LiberKey\MyApps\Sublime Text 2\Data\Packages"
-Function create_settings {
+
+#Functions
+Function Create-Settings {
   Write-Output "{" > "tmp_prefs.sublime-settings"
   Get-Content "preferences\Preferences.sublime-settings" >> "tmp_prefs.sublime-settings"
   Get-Content "windows\Preferences.sublime-settings" >> "tmp_prefs.sublime-settings"
   Write-Output "}" >> "tmp_prefs.sublime-settings"
-  Get-Content "tmp_prefs.sublime-settings" > $sublime_packages_path"\User\Preferences.sublime-settings"
-  rm "tmp_prefs.sublime-settings"
+  $path = $sublime_packages_path+"\User\Preferences.sublime-settings"
+  $my_file = Get-Content "tmp_prefs.sublime-settings"
+  [System.IO.File]::WriteAllLines($path , $my_file )
+  Remove-Item "tmp_prefs.sublime-settings"
 }
 
-
+# Script Start
 Write-Output "This is a script to install some sublime packages, snippets and settings."
 $continue =  Read-Host "Do you want to install the settings file? [Yes to continue, no to abort]"
 if ($continue -ne "Yes" -and $continue -ne "Y" -and $continue-ne "y") {
@@ -19,5 +24,5 @@ $sublime_packages_path = Read-Host "Where is your sublime Packages path? [$defau
 if ($sublime_packages_path -eq "") {
   $sublime_packages_path = $default_sublime_packages_path
 }
-create_settings
+Create-Settings
 
