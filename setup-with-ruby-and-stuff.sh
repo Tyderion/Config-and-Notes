35336e704ad29f2942660a5d576635f10c048c50
Tyderion <nadlergabriel@gmail.com>
2012-12-08 03:03:00 +0100

    New extract() function (replaces ex())

--- setup-with-ruby-and-stuff.sh
+++ setup-with-ruby-and-stuff.sh
@@ -84,29 +84,27 @@ export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 
 
 #Expand-o-Matic:
-ex () {
-  if [ "$1" == '' ] ; then
-	echo "This is the Expand-o-Matic"
-  elif [ -f $1 ] ; then
-    case $1 in
-      *.tar.bz2)   tar xjf $1        ;;
-      *.tar.gz)    tar xzf $1     ;;
-      *.bz2)       bunzip2 $1       ;;
-      *.rar)       rar x $1     ;;
-      *.gz)        gunzip $1     ;;
-      *.tar)       tar xf $1        ;;
-      *.tbz2)      tar xjf $1      ;;
-      *.tgz)       tar xzf $1       ;;
-      *.zip)       unzip $1     ;;
-      *.Z)         uncompress $1  ;;
-      *.7z)        7z x $1    ;;
-      *)           echo "'$1' cannot be extracted via ex()" ;;
-    esac
-  else
-    echo "'$1' is not a valid file"
-  fi
+function extract() # Handy Extract Program.
+{
+ if [ -f $1 ] ; then
+ case $1 in
+ *.tar.bz2) tar xvjf $1 ;;
+ *.tar.gz) tar xvzf $1 ;;
+ *.bz2) bunzip2 $1 ;;
+ *.rar) unrar x $1 ;;
+ *.gz) gunzip $1 ;;
+ *.tar) tar xvf $1 ;;
+ *.tbz2) tar xvjf $1 ;;
+ *.tgz) tar xvzf $1 ;;
+ *.zip) unzip $1 ;;
+ *.Z) uncompress $1 ;;
+ *.7z) 7z x $1 ;;
+ *) echo "'$1' cannot be extracted via &gt;extract&lt;" ;;
+ esac
+ else
+ echo "'$1' is not a valid file"
+ fi
 }
-
 
 #Bashrc
 #Functions to start graphical programs in the back
