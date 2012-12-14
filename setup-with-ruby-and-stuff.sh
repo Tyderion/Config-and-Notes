34f89717dec23680248dd5600d34efd6a67b6e31
Gabriel Nadler <nadlergabriel@gmail.com>
2012-12-14 14:30:17 +0100

    Fixed Mergeconflicts

--- setup-with-ruby-and-stuff.sh
+++ setup-with-ruby-and-stuff.sh
@@ -3,31 +3,12 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get -y install build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion pkg-config curl guake
 
-
+echo "Installing Sublime Text 2"
 wget "http://c758482.r82.cf2.rackcdn.com/Sublime Text 2.0.1 x64.tar.bz2"
 tar xf Sublime\ Text\ 2\ Build\ 2181\ x64.tar.bz2
 sudo mv Sublime\ Text\ 2 /usr/lib/
 sudo ln -s /usr/lib/Sublime\ Text\ 2/sublime_text /usr/bin/sublime
-cat << 'sublime' | sudo tee -a  /usr/share/applications/sublime.desktop
-[Desktop Entry]
-Version=1.0
-Name=Sublime Text 2
-# Only KDE 4 seems to use GenericName, so we reuse the KDE strings.
-# From Ubuntu's language-pack-kde-XX-base packages, version 9.04-20090413.
-GenericName=Text Editor
-
-Exec=sublime
-Terminal=false
-Icon=/usr/lib/Sublime Text 2/Icon/48x48/sublime_text.png
-Type=Application
-Categories=TextEditor;IDE;Development
-X-Ayatana-Desktop-Shortcuts=NewWindow
-
-[NewWindow Shortcut Group]
-Name=New Window
-Exec=sublime -n
-TargetEnvironment=Unity
-sublime
+cat sublime_desktop | sudo tee -a  /usr/share/applications/sublime.desktop
 
 #make bash changes
 cat inputrc_tweaks | sudo tee -a /etc/inputrc
